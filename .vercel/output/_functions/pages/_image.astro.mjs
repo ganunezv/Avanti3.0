3cb2ca9cbaf3e69a0d82b873df147b7334f64c19
Gustavo <ganunezv@gmail.com>
2025-04-10 19:00:12 -0500

    fix: astro config

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_BS-rnBFJ.mjs';
-export { renderers } from '../renderers.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  output: "server",
+  output: "static",
   adapter: vercel({
     includeFiles: ["./my-data.json"],
     webAnalytics: {
